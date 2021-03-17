2b6cf93d60ab8be7d116782c699a1e4426cd7ece
Wilson Loayza <wilson_loayza@hotmail.com>
2021-03-17 17:18:13 +0100

    up

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Full-Stack Developer", "Desarrollador Web", "Freelancer"],
+    strings: ["FullStack Developer", "Desarrollador Web", "Freelancer"],
     typeSpeed: 150,
     backSpeed: 60,
     loop: true,
