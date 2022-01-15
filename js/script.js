e2249daa44ce731f84dd7fe888d87e0a034c4d02
Wilson Loayza <wilson_loayza@hotmail.com>
2022-01-15 11:47:04 +0100

    quitar freelancer

--- js/script.js
+++ js/script.js
@@ -35,7 +35,7 @@ $(document).ready(function () {
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["FullStack Developer", "Desarrollador Web", "Freelancer"],
+    strings: ["FullStack Developer", "Desarrollador Web FullStack"],
     typeSpeed: 150,
     backSpeed: 60,
     loop: true,
